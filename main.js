150b818b6edbbf227e1f9ba93474c84fd1c56666
AndriiKot <a_kocyuba@ukr.net>
2023-03-26 13:49:54 +0200

    Add box-shadow in function box-size

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function box_size(){
   d.forEach (d => {
   d.style.width = max_width / 4.2655+'px';
   d.style.height = max_height / 1.8 +'px';
+  d.style.boxShadow = `0 0 0 ${max_width / 4.2655 / 15}px #070c23`
   });
   d = document.querySelector('body')
   d.style.gap = max_width / 6.4 + 'px';
