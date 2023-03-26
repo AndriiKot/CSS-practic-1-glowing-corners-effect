76e96ac4da0365e9f3f08ec7f69dec8932a4d367
AndriiKot <a_kocyuba@ukr.net>
2023-03-26 13:40:09 +0200

    Modif function box-size and add gap css

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ function box_size(){
   d.forEach (d => {
   d.style.width = max_width / 4.2655+'px';
   d.style.height = max_height / 1.8 +'px';
-  })
+  });
+  d = document.querySelector('body')
+  d.style.gap = max_width / 6.4 + 'px';
 }
 
